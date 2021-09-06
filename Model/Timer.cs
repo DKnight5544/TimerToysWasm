292fa698e2b54365e042dcdbb25473539be66c93
DKnight5544 <dave@mostlyspam.com>
2021-09-06 09:43:22 -0700

    Add project files.

---
+++ Model/Timer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TimerToysWasm.Model
+{
+	public class Timer
+	{
+		public string PageKey { get; set; }
+		public string PageName { get; set; }
+		public string TimerKey { get; set; }
+		public string TimerDescription { get; set; }
+		public bool IsRunning { get; set; }
+		public int SortIndex { get; set; }
+		public int ElapsedTime { get; set; }
+	}
+
+
+}
